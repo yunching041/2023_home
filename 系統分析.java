9e4d23d26e198aba2bb1508998e79256cb4c772a
yunching041 <145169934+yunching041@users.noreply.github.com>
2023-11-01 15:50:16 +0800

    Update 系統分析.java

--- 系統分析.java
+++ 系統分析.java
@@ -7,3 +7,21 @@ abstract class CShape{
 
     public abstract void show();
 }
+
+
+
+class CTriangle extends CShape{
+    double ca, cb, cc;
+    public CTriangle(double a, double b, double c){
+        ca=a;
+        cb=b;
+        cc=c;
+    }
+   
+    public void show() {
+       
+        System.out.print("color="+color+"  ");
+        System.out.print("area="+0.5*ca*cb);
+    }
+   
+}
