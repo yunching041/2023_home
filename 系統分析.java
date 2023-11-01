7aed0ae00085181bc58c48e5e98af2da4a5920f6
yunching041 <145169934+yunching041@users.noreply.github.com>
2023-11-01 15:51:44 +0800

    Update 系統分析.java

--- 系統分析.java
+++ 系統分析.java
@@ -1,3 +1,4 @@
+
 abstract class CShape{
     protected String color;
     public void setColor(String str){
@@ -25,3 +26,64 @@ class CTriangle extends CShape{
     }
    
 }
+
+
+public class Student {
+    import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+    private String name;
+    private List<Course> courses;
+
+    public Student(String name) {
+        this.name = name;
+        this.courses = new ArrayList<>();
+    }
+
+    public boolean addCourse(Course course) {
+        if (courses.size() < 10) {
+            courses.add(course);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean removeCourse(Course course) {
+        return courses.remove(course);
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    // Other getters, setters, and relevant methods
+}
+
+public class Course {
+    private String courseName;
+
+    public Course(String courseName) {
+        this.courseName = courseName;
+    }
+
+    // Getters, setters, and other relevant methods
+}
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
