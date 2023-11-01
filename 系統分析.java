7ede0e8322b74a8f0eddbe2c553c3e8c5dc2e2aa
yunching041 <145169934+yunching041@users.noreply.github.com>
2023-11-01 15:52:39 +0800

    Update 系統分析.java

--- 系統分析.java
+++ 系統分析.java
@@ -73,7 +73,13 @@ public class Course {
     
 }
 
-
+public class app11 {
+   public static void main(String[] args) {
+    CTriangle ct = new CTriangle(3, 4, 5);
+    ct.setColor("red");
+    ct.show();
+    }
+}
 
 
 
